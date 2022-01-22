07e082bb716b18a4d32689e669373b20baab8822
Raushan Kumar <60419872+raushanapp@users.noreply.github.com>
2022-01-22 17:53:28 +0530

    js and html files raushan

---
+++ mobile.js
@@ -0,0 +1,45 @@
+
+// sliding js  start 
+
+  let count = 1
+
+  setInterval(()=>{
+    document.getElementById('radio' + count).checked = true;
+    count++
+
+    if (count>7) {
+        count = 1
+    }
+  },5000)
+// sliding js  end
+
+
+// shop by price start js 
+
+// showSlider()
+
+// shop by price end js 
+let arrData = [];
+
+let addCartRaus = () =>{
+   
+  let cartData = {
+    // image:document.getElementsByClassName('imgaeLimitedRaus').src,
+    // title:document.getElementsByClassName('TitleRaus').innertext,
+    // price:document.getElementsByClassName('timeDealeRaus').innertext
+  }
+  console.log("click me")
+ let  name = document.getElementById('titlesRaus5').textContent
+  console.log(name)
+
+  
+
+
+}
+// addto cart start
+
+
+  
+// addto cart end
+
+
